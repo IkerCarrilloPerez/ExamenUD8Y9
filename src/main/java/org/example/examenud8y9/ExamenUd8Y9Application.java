59d056d3fb9367f5ebf2394add80e9286c745e42
IkerCarrilloPerez <ikercarrilloperez@gmail.com>
2026-03-18 08:38:35 +0100

    Initial commit

---
+++ src/main/java/org/example/examenud8y9/ExamenUd8Y9Application.java
@@ -0,0 +1,13 @@
+package org.example.examenud8y9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenUd8Y9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenUd8Y9Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/examenud8y9/ExamenUd8Y9ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.examenud8y9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenUd8Y9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
